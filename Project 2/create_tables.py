b7ea582b790766abcf225f65f01a7c9c94b2f080
Sangwon Lee <samlee0326@gmail.com>
2023-02-15 02:34:58 +0900

    Update create_tables.py

--- Project 2/create_tables.py
+++ Project 2/create_tables.py
@@ -46,7 +46,7 @@ def main():
         - new tables in the AWS Clusters
     """
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read('./config/dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     print('Connected')
